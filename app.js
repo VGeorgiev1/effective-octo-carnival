0594d68614509ec19c00a7f984439041d15f82f6
Vladislav <vladdislav.georgiev@gmail.com>
2017-09-18 16:52:54 +0300

    added shit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 app.set('view engine', 'pug');
+const path=require('path');
+app.use(express.static(path.join(path.normalize(path.join(__dirname, '/../')), 'public')));
 
 app.get('/', function (req,res) {
     res.render('home');
@@ -8,7 +10,6 @@ app.get('/', function (req,res) {
 app.get('/register', function (req,res) {
     res.render('register');
 });
-
 app.listen(3000, function () {
     console.log('Ready!');
 });
