42564c6e99c2b160c420df21a0b7da3ee503908e
mdatsev <martin.datsev.p@gmail.com>
2017-09-18 16:56:50 +0300

    app.use(express.static('public')); instead of app.use(express.static(path.join(path.normalize(path.join(__dirname, '/../')), 'public')));

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 app.set('view engine', 'pug');
-const path=require('path');
-app.use(express.static(path.join(path.normalize(path.join(__dirname, '/../')), 'public')));
+const path = require('path');
 
-app.get('/', function (req,res) {
+
+app.get('/', function(req, res) {
     res.render('home');
 });
-app.get('/register', function (req,res) {
+app.get('/register', function(req, res) {
     res.render('register');
 });
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log('Ready!');
 });
+
+//app.use(express.static(path.join(path.normalize(path.join(__dirname, '/../')), 'public')));
+app.use(express.static('public'));
