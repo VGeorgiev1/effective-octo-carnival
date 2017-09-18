b3775cc0fef5b0b381f31d0d307a3fd8d697add7
Vladislav <vladdislav.georgiev@gmail.com>
2017-09-18 17:44:30 +0300

    added login post

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
+const appKey=''
 app.set('view engine', 'pug');
 const path = require('path');
-
-
+const bodyParser=require('body-parser');
+let $ = require('jquery');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.get('/', function(req, res) {
+    console.log('wtf');
     res.render('home');
 });
 app.get('/register', function(req, res) {
     res.render('register');
 });
-app.listen(3000, function() {
+app.post('/login', function (req,res) {
+   console.log(req.body);
+});
+app.listen(300, function() {
     console.log('Ready!');
 });
 
