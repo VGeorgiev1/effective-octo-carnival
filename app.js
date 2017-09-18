38da68919052b333033a6190db7de4bb886f1837
Vladislav <vladdislav.georgiev@gmail.com>
2017-09-18 23:13:58 +0300

    added forum posts

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ var Kinvey = require('kinvey-node-sdk');
 Kinvey.initialize({
     appKey: 'kid_SJg4EY6cW',
     appSecret: 'e3b622e5dd8e468da97e3fcc2366860a'
-}).then(function(activeUser) {
-    // ...
-}).catch(function(error) {
-    // ...
-})
+});
 
 app.set('view engine', 'pug');
 
@@ -22,70 +18,84 @@ app.get('/', function(req, res) {
 });
 app.get('/register', function(req, res) {
     res.render('register');
-
 });
 app.post('/register', function(req, res) {
-    console.log(req.body);
     let user = new Kinvey.User();
     let promise;
+
     switch (req.body.role) {
         case 'student':
             promise = user.signup({
                 username: req.body.user_email,
+                name: req.body.user_name,
                 password: req.body.user_password,
                 role: req.body.role,
                 class: req.body.class,
                 grade: req.body.grade
-
-            }).then(function (user) {
-                res.render('main')
-
-            })
+            }).then(function(user) {
+                res.redirect('/main');
+            });
             break;
         case 'teacher':
             promise = user.signup({
                 username: req.body.user_email,
                 password: req.body.user_password,
+                name: req.body.user_name,
                 role: req.body.role,
-            }).then(function (user) {
-                 res.render('main');
-
                 subjects: Object.keys(req.body).filter(function(k) {
                     return k.indexOf('subject') == 0;
                 }).reduce(function(newData, k) {
                     newData.push(req.body[k]);
                     return newData;
                 }, [])
+            }).then(function(user) {
+
+                res.redirect('/main');
             });
             break;
         case 'parent':
             promise = user.signup({
                 username: req.body.user_email,
                 password: req.body.user_password,
+                name: req.body.user_name,
                 role: req.body.role,
-            }).then(function (user) {
-                res.render('main');
-
                 childmail: Object.keys(req.body).filter(function(k) {
                     return k.indexOf('childmail') == 0;
                 }).reduce(function(newData, k) {
                     newData.push(req.body[k]);
                     return newData;
                 }, [])
-            })
+            }).then(function(user) {
+                res.redirect('/main');
+            });
             break;
     }
 
 });
 app.post('/login', function(req, res) {
-    console.log('zxr');
-    console.log(req.body);
+
+
     let promise = Kinvey.User.login({
         username: `${req.body.user_email}`,
         password: `${req.body.user_password}`
     }).then(function (user) {
-        res.render('main')
+        res.redirect('/main');
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+app.get('/main', function (req,res) {
 
+    let threadStore = Kinvey.DataStore.collection('threads');
+    let stream = threadStore.find();
+    stream.subscribe(function onNext(entities) {
+
+        res.render('main', {ent:entities})
+    }, function onError(error) {
+        console.log(error);
+    }, function onComplete() {
+
+    });
 });
 app.get('/logout', function (req,res) {
     let promise = Kinvey.User.logout()
@@ -93,9 +103,24 @@ app.get('/logout', function (req,res) {
             res.render('home');
         })
 });
+app.post('/post', function (req,res) {
+    let threadStore = Kinvey.DataStore.collection('threads');
+    var activeUser = Kinvey.User.getActiveUser();
+    let promiseUser = Promise.resolve(activeUser);
+    promiseUser.then(function (activeuser) {
+
+    let promise = threadStore.save({
+        text: `${req.body.text}`,
+        author:`${activeuser.data.name}`
+    }).then(function onSuccess(entity) {
+        res.redirect('/main');
+    }).catch(function (err) {
+        console.log(err);
+    });
+    });
+
+});
 app.listen(300, function() {
     console.log('Ready!');
 });
-
-//app.use(express.static(path.join(path.normalize(path.join(__dirname, '/../')), 'public')));
 app.use(express.static('public'));
