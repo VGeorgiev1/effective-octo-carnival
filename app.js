9b10f9ff4b50fe2625af17f17dc5d9a3e5f7f704
Vladislav <vladdislav.georgiev@gmail.com>
2017-09-18 16:13:26 +0300

    adding jade as template engine

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const app=express();
+app.set('view engine', 'pug');
 
 app.get('/', function (req,res) {
-   r
-
+    res.render('home');
+});
+app.get('/register', function (req,res) {
+    res.render('register');
 });
 
 app.listen(3000, function () {
